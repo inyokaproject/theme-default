291572f77a045457042c3e9cd82cec05befa8892
Lyra <lyra2108@gmail.com>
2015-09-20 20:57:42 +0200

    Remove code in comment in base.js
    It was inside a comment, so it was newer used, so we don't need it.

--- inyoka_theme_default/static/js/base.js
+++ inyoka_theme_default/static/js/base.js
@@ -1,32 +1,4 @@
 $(function () {
-/*
-    function sidebar_resize() {
-        var width = $('.col-sm-3').width();
-        var padding_left = parseFloat($('#sidebar').css('padding-left').replace('px', ''));
-        var padding_right = parseFloat($('#sidebar').css('padding-right').replace('px', ''));
-        width -=  padding_left + padding_right;
-        $('#sidebar').width(width);
-    };
-
-    $(window).scroll(function () {
-        var y = $(window).scrollTop();
-        if (y > 0) {
-            $("#main-navbar").addClass('shadowed');
-        } else {
-            $("#main-navbar").removeClass('shadowed');
-        }
-    });
-
-    $(window).resize(function () {
-        sidebar_resize();
-    });
-    var navbar_height = parseFloat($('#main-navbar').css('height'), 10) * 1.38;
-    $('#sidebar').affix({
-        offset: { top: $('#sidebar').offset().top - navbar_height }
-    });
-
-    sidebar_resize();
-*/
     if (window.location.hash) {
         var pos = $(window.location.hash).offset().top - navbar_height;
         window.setTimeout(function () {

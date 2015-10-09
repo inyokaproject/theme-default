9d34c8b691b8707f074ff4e5bbd9c5e327120ace
Christoph Volkert <inyoka@christoph-volkert.de>
2015-10-09 13:14:21 +0200

    Fixed focus-conflict between smoothscrolling and comment-field

--- inyoka_theme_default/static/js/smoothscrolling.js
+++ inyoka_theme_default/static/js/smoothscrolling.js
@@ -3,6 +3,10 @@
  * ~~~~~~~~~~~~~~~~~~
  * 
  * Smoothly scroll to ids inside a document. Script includes additions for a11y.
+ *
+ * the tabindex and focus on the target-element can be disabled via
+ * `data-focus-after-scroll="false"` on the link
+ *
  * 
  * source: http://codepen.io/theandyyates/pen/dGovD/
  * (thus MIT licensed, see https://blog.codepen.io/legal/licensing/)
@@ -11,18 +15,23 @@
  * :license: BSD, see LICENSE for more details.
  */
 
-$(function() {
+$(document).ready(function() {
   $('a[href*=#]:not([href=#])').click(function(event) {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var focus_data = $(this).attr('data-focus-after-scroll');
+      var focus_after_scroll = focus_data ? (focus_data === "true") : true;
+
       target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
-        }, 875, function(){
-          target.attr('tabindex', '-1');
-          target.focus();
-          });
+        }, 875, function() {
+          if(focus_after_scroll) {
+            target.attr('tabindex', '-1');
+            target.focus();
+          }
+        });
       location.hash = target;
       return false;
       }
